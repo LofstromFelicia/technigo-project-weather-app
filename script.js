114ba3e81aa1aa375c3c256e455cdeada13fad57
Felicia Löfström <Flizan_Linnea@hotmail.com>
2026-05-17 12:59:09 +0200

    add: weather based background

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const city = document.getElementById("city")
 const temperature = document.getElementById("temperature")
 const description = document.getElementById("description")
 
+const styleWeatherApp = (weatherMain) => {
+  const container = document.getElementById("weather-container")
+  const outfitTip = document.getElementById("outfit-tip")
+
+  // Function that styles app based on weather-id & Outfit tip (kläder efter väder)
+  if (weatherMain === "Clear") {
+    container.style.backgroundColor = "#F7DC6F" //sunny yellow
+    outfitTip.innerHTML = "Kläder efter väder: Solbrillor och T-shirt! ☀️"
+  } else if (weatherMain === "Clouds") {
+    container.style.backgroundColor = "#AEB6BF" //cloudy grey 
+    outfitTip.innerHTML = "Kläder efter väder: En skön tröja räcker nog. ☁️"
+  } else if (weatherMain === "Thunderstorm") {
+    container.style.backgroundColor = "#2C3E50" //thunder grey
+    outfitTip.innerHTML = "Kläder efter väder: Håll dig inomhus i myskläder! ⛈️"
+  } else if (weatherMain === "Rain" || weatherMain === "Drizzle") {
+    container.style.backgroundColor = "#34495E" //rain grey
+    outfitTip.innerHTML = "Kläder efter väder: Regnjacka och gummistövlar på! 🌧️"
+  } else if (weatherMain === "Snow") {
+    container.style.backgroundColor = "#EBF5FB" //snow white
+    outfitTip.innerHTML = "Kläder efter väder: Tjockjacka, mössa och vantar! ❄️"
+  }
+}
+
 const API_KEY = "b41cc812b4058189c1534f5dd668317d"
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&lang=se&APPID=${API_KEY}`
 
@@ -12,12 +35,15 @@ const fetchWeather = () => {
       console.log("Weatherdata:", data)
 
       city.innerHTML = data.name
-
       temperature.innerHTML = `${data.main.temp.toFixed(1)}°`
-
       description.innerHTML = data.weather[0].description
+
+      const weatherMain = data.weather[0].main
+
+      styleWeatherApp(weatherMain)
     })
     .catch((error) => console.error("Oops, your weather fetch did not work:", error))
 }
 
 fetchWeather()
+
